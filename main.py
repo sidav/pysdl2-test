23b4c0fd1d3626bbe0fec9a38b55af80c5e47610
sidav <sidav94@gmail.com>
2018-03-18 17:19:23 +0300

    Moar functionality to the wrapper.

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ draw.init()
 # draw.line(1, 1, 100, 200)
 # draw.circle(0, 0, 250)
 shp = Ship()
-Renderer.render_ship(shp)
-draw.flush_screen()
-draw.proc_fuck()
+draw.set_color(128, 0, 200)
+while not draw.is_window_closed():
+    Renderer.render_ship(shp)
+    Renderer.zoom_factor *= 1.00001
+    draw.flush_screen()
+    draw.clear_screen()
--- sdl2_wrapper.py
+++ sdl2_wrapper.py
@@ -1,9 +1,12 @@
+import ctypes
 import sdl2.ext as sdl
+import sdl2
 
 window_width = window_height = 0
 renderer = None
 window = None
 current_color = sdl.Color(255, 255, 255)
+evh = None
 
 
 def init(windowname='ZOMG FORGOT TO NAME IT', width=320, height=200):
@@ -14,26 +17,43 @@ def init(windowname='ZOMG FORGOT TO NAME IT', width=320, height=200):
     window = sdl.Window(windowname, size=(window_width, window_height))
     window.show()
     renderer = sdl.Renderer(window)
+    # renderer = sdl2.SDL_CreateRenderer(
+    #     window,
+    #     -1,
+    #     sdl2.SDL_RENDERER_ACCELERATED | sdl2.SDL_RENDERER_PRESENTVSYNC
+    #     )
+    # sdl.
+
+def is_window_closed():
+    event = sdl2.SDL_Event()
+    if sdl2.SDL_PollEvent(ctypes.byref(event)) != 0:
+        if event.type == sdl2.SDL_QUIT:
+            return True
+    return False
 
 
-def proc_fuck():
-    processor = sdl.TestEventProcessor()
-    processor.run(window)
+def clear_screen(color=sdl.Color(0, 0, 0)):
+    renderer.clear(color)
 
 
 def flush_screen():
     renderer.present()
 
 
+def set_color(r, g, b):
+    global current_color
+    current_color = sdl.Color(r, g, b)
+
+
 def pixel(x, y):
-    renderer.draw_point([x, y])
+    renderer.draw_point([x, y], current_color)
 
 
 def line(x1, y1, x2=-1, y2=-1):
     if x2 == y2 == -1:
-        renderer.draw_line([int(x1[0]), int(x1[1]), int(y1[0]), int(y1[1])]) # what a shame...
+        renderer.draw_line([int(x1[0]), int(x1[1]), int(y1[0]), int(y1[1])], current_color) # what a shame...
     else:
-        renderer.draw_line([int(x1), int(y1), int(x2), int(y2)])
+        renderer.draw_line([int(x1), int(y1), int(x2), int(y2)], current_color)
 
 
 def circle(x0, y0, radius):
