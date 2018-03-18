8037f5a4e486955eb78d75dbb5bddd5f0c0b601e
sidav <sidav94@gmail.com>
2018-03-18 14:19:32 +0300

    Added algorithms to the wrapper.

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import sdl2_wrapper as draw
 draw.init()
 
 # renderer.
-draw.line(1,1,100,200)
+draw.line(1, 1, 100, 200)
+draw.circle(100, 100, 25)
 draw.flush_screen()
+draw.proc_fuck()
--- sdl2_wrapper.py
+++ sdl2_wrapper.py
@@ -2,6 +2,7 @@ import sdl2.ext as sdl
 
 renderer = None
 window = None
+current_color = sdl.Color(255, 255, 255)
 
 
 def init(windowname='ZOMG FORGOT TO NAME IT', width=320, height=200):
@@ -10,13 +11,46 @@ def init(windowname='ZOMG FORGOT TO NAME IT', width=320, height=200):
     window = sdl.Window(windowname, size=(width, height))
     window.show()
     renderer = sdl.Renderer(window)
-    # processor = sdl.TestEventProcessor()
-    # processor.run(window)
+
+
+def proc_fuck():
+    processor = sdl.TestEventProcessor()
+    processor.run(window)
 
 
 def flush_screen():
     renderer.present()
 
 
+def pixel(x, y):
+    renderer.draw_point([x, y])
+
+
 def line(x1, y1, x2, y2):
     renderer.draw_line([x1, y1, x2, y2])
+
+
+def circle(x0, y0, radius):
+    x = radius
+    y = 0
+    dx = 1
+    dy = 1
+    err = dx - (radius * 2)
+    while x >= y:
+        pixel(x0 + x, y0 + y)
+        pixel(x0 + y, y0 + x)
+        pixel(x0 - y, y0 + x)
+        pixel(x0 - x, y0 + y)
+        pixel(x0 - x, y0 - y)
+        pixel(x0 - y, y0 - x)
+        pixel(x0 + y, y0 - x)
+        pixel(x0 + x, y0 - y)
+
+        if err <= 0:
+            y += 1
+            err += dy
+            dy += 2
+        if err > 0:
+            x -= 1
+            dx += 2
+            err += dx - (radius * 2)
